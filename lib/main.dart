9186a6d561ebefea671c2ce6d5319e27e35fa737
Martin Rouede <martinrouede1@gmail.com>
2021-05-13 20:21:38 -0300

    se agregan funciones y se eliminan las que ya no se usan

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'theme_changer.dart';
 import 'add_prduct.dart';
 import 'list_prducts.dart';
 import 'send_load_list.dart';
@@ -16,47 +18,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Market List',
-      localizationsDelegates: [
-        AppLocalizations.delegate, // Add this line
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        const Locale('en', ''), // English, no country code
-        const Locale('es', ''), // Spanish, no country code
-      ],
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+    return ThemeBuilder(
+      defaultBrightness: Brightness.dark,
+      builder: (context, _brightness) {
+        return MaterialApp(
+          title: 'Market List',
+          localizationsDelegates: [
+            AppLocalizations.delegate, // Add this line
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: [
+            const Locale('en', ''), // English, no country code
+            const Locale('es', ''), // Spanish, no country code
+          ],
+          theme: ThemeData(primarySwatch: Colors.blue, brightness: _brightness),
+          home: MyHomePage(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -71,17 +58,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _addItemToList() {
     setState(() {
-      var aProduct = new Product();
-      aProduct.name = _newProductController.text;
-      aProduct.isSelected = false;
-      _someProducts.insert(0, aProduct);
-      _newProductController.text = '';
-    });
-  }
-
-  void _clearList() {
-    setState(() {
-      _someProducts.clear();
+      if (_newProductController.text.isNotEmpty) {
+        var aProduct = new Product();
+        aProduct.name = _newProductController.text;
+        aProduct.isSelected = false;
+        _someProducts.insert(0, aProduct);
+        _newProductController.text = '';
+      }
     });
   }
 
@@ -110,20 +93,40 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _removeItem(index) {
+    setState(() {
+      _someProducts.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(AppLocalizations.of(context).marketList),
-        ),
+            title: Text(AppLocalizations.of(context).marketList),
+            actions: <Widget>[
+              if (ThemeBuilder.of(context).getCurrentTheme() == Brightness.dark)
+                IconButton(
+                    icon: FaIcon(FontAwesomeIcons.moon),
+                    onPressed: () {
+                      ThemeBuilder.of(context).changeTheme();
+                    })
+              else
+                IconButton(
+                    icon: FaIcon(FontAwesomeIcons.sun),
+                    onPressed: () {
+                      ThemeBuilder.of(context).changeTheme();
+                    }),
+            ]),
         body: Padding(
             padding: EdgeInsets.all(20),
             child: Column(children: <Widget>[
               AddProduct(
-                  addItem: _addItemToList,
-                  controller: _newProductController,
-                  clearList: _clearList),
-              ListProducts(products: _someProducts, selectItem: _selectItem),
+                  addItem: _addItemToList, controller: _newProductController),
+              ListProducts(
+                  products: _someProducts,
+                  selectItem: _selectItem,
+                  removeItem: _removeItem),
               SendLoadList(
                   products: _someProducts,
                   controller: _listController,
